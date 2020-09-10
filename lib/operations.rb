92aebe0388d16f927f0e93c85129321412288c16
Alex Darvin <amdarvin14@gmail.com>
2020-09-10 18:08:16 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def unsafe?(speed)
   if speed > 60 || speed < 40
     true
@@ -8,6 +9,16 @@ end
 
 def not_safe?(speed)
 	speed >60 || speed < 40 ? true : false
+=======
+def unsafe(speed)
+  if speed > 60 && speed < 40
+  else safe
+end
+
+
+def not_safe?(speed)
+	puts not_safe ? speed > 60 && speed <40 : safe
+>>>>>>> a6d164943d26fe1a3065c36ef875f47153682b82
 end
 	
 
